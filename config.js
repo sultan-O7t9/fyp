da1190bc5e9780b993f1e42f8084134d149b2161
sultan-O7t9 <sm.7249000@gmail.com>
2022-02-03 10:47:31 +0500

    reorganized as a fullstack app

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  host: "localhost",
-  user: "root",
-  password: "$ult@n123",
-  insecureAuth: true,
-  database: "schema_fyp",
-};
--- server.js
+++ server.js
@@ -1,37 +1,17 @@
-//Imports
-// import express from "express";
-// import cors from "cors";
-// import dotenv, { config } from "dotenv";
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const mysql = require("mysql");
-const dbConfig = require("./config.js");
 //Init
 const app = express();
 app.use(cors());
 dotenv.config();
-const PORT = process.env.PORT || 5000;
 
-//Routes
+const port = process.env.PORT || 8080;
+
 app.get("/", (req, res) => {
-  res.send({ msg: "Hello World" });
+  res.send("Hello World!");
 });
 
-const connection = mysql.createConnection(dbConfig);
-
-connection.connect(function (err) {
-  if (err) {
-    return console.error("error: " + err.message);
-  }
-
-  console.log("Connected to the MySQL server.");
-});
-
-let sql = "SELECT * FROM `tbl_department`";
-connection.query(sql, (error, results, fields) => {
-  if (error) {
-    return console.error(error.message);
-  }
-  console.log(results);
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
