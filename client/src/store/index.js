a19b2ab6a1fda9527b14061ce480c48f964b0241
sultan-O7t9 <sm.7249000@gmail.com>
2022-02-05 19:06:57 +0500

    Create User

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,7 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import ReduxThunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import studentsReducer from "./reducers/students";
 
 const rootReducer = combineReducers({
