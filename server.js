8a3d2ef9ffc46cc0bea097f87da53a030e4fa536
sultan-O7t9 <sm.7249000@gmail.com>
2022-02-06 22:21:58 +0500

    added env to gitignore but its not working as of now :/

--- server.js
+++ server.js
@@ -8,9 +8,13 @@ dotenv.config();
 
 const port = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
