449a43afc0f3bd0434162a51166cc4c143b34401
sultan-O7t9 <sm.7249000@gmail.com>
2022-02-09 12:50:13 +0500

    ready for deployment

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 //Init
 const app = express();
 app.use(cors());
 dotenv.config();
+//Init DB
+require("./models");
 
 const port = process.env.PORT || 8080;
 
@@ -13,8 +16,12 @@ const port = process.env.PORT || 8080;
 // });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client", "build")));
 }
+
+app.get("*", (req, res) => {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
